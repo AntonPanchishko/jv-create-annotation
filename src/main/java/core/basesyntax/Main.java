d92ddfe7b0557ad900c4d6e02f9666f94f683f35
antonpanchishko <PanchishkoMayor@gmail.com>
2021-01-10 21:29:10 +0200

    4) added factory and injections
    (cherry picked from commit 3a1dab198eefd1342937501ea7bd8ddaa22c4838)

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -1,11 +1,19 @@
 package core.basesyntax;
 
 import core.basesyntax.controler.ConsoleHandler;
+import core.basesyntax.dao.BetDao;
+import core.basesyntax.factory.Factory;
+import core.basesyntax.lib.Injector;
+import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-    public static void main(String[] args) {
-        ConsoleHandler consoleHandler = new ConsoleHandler();
+    public static void main(String[] args) throws InvocationTargetException,
+            NoSuchMethodException, InstantiationException, IllegalAccessException {
+        ConsoleHandler consoleHandler = (ConsoleHandler) Injector
+                .getInstance(ConsoleHandler.class);
         System.out.println("Enter value and risk");
-        consoleHandler.userHandler();
+        consoleHandler.betHandler();
+        BetDao betDao = Factory.getBetDao();
+        System.out.println("All bets: " + betDao.getAllBets());
     }
 }
