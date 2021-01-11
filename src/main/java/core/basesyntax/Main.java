cadc21661cb076963e89b99635c1624e12121bdc
antonpanchishko <PanchishkoMayor@gmail.com>
2021-01-11 13:14:09 +0200

    5) added custom exception
    (cherry picked from commit 2ac4c5d992cad0fb3f75cba6cce4fab411914fb4)

--- src/main/java/core/basesyntax/Main.java
+++ src/main/java/core/basesyntax/Main.java
@@ -2,6 +2,7 @@ package core.basesyntax;
 
 import core.basesyntax.controler.ConsoleHandler;
 import core.basesyntax.dao.BetDao;
+import core.basesyntax.dao.UserDao;
 import core.basesyntax.factory.Factory;
 import core.basesyntax.lib.Injector;
 import java.lang.reflect.InvocationTargetException;
@@ -11,9 +12,14 @@ public class Main {
             NoSuchMethodException, InstantiationException, IllegalAccessException {
         ConsoleHandler consoleHandler = (ConsoleHandler) Injector
                 .getInstance(ConsoleHandler.class);
-        System.out.println("Enter value and risk");
+        /*System.out.println("Enter value and risk");
         consoleHandler.betHandler();
         BetDao betDao = Factory.getBetDao();
-        System.out.println("All bets: " + betDao.getAllBets());
+        System.out.println("All bets: " + betDao.getAllBets());*/
+
+        System.out.println("Enter you name and lastname");
+        consoleHandler.userHandler();
+        UserDao userDao = Factory.getUserDao();
+        System.out.println("All users: " + userDao.getAllUsers());
     }
 }
