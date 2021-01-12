b3a140f78b43ff83cddffd597cf73927b99d5fcf
antonpanchishko <PanchishkoMayor@gmail.com>
2021-01-12 14:35:50 +0200

    Revert "6) fixed getInstance"
    This reverts commit 77f9fccc

--- src/main/java/core/basesyntax/lib/Injector.java
+++ src/main/java/core/basesyntax/lib/Injector.java
@@ -4,30 +4,41 @@ import core.basesyntax.dao.BetDao;
 import core.basesyntax.dao.UserDao;
 import core.basesyntax.exception.WrongAnnotationException;
 import core.basesyntax.factory.Factory;
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.function.Predicate;
 
-public class Injector {
+public class Injector implements Inject {
+
     public static Object getInstance(Class clazz) throws NoSuchMethodException,
             IllegalAccessException, InvocationTargetException, InstantiationException {
         Constructor constructor = clazz.getDeclaredConstructor();
         Object instance = constructor.newInstance();
         Field[] declaredFields = clazz.getDeclaredFields();
         for (Field field: declaredFields) {
-            field.setAccessible(true);
-            Object currObj = new Object();
-            if (field.getType() == BetDao.class) {
-                currObj = Factory.getBetDao();
-            } else if (field.getType() == UserDao.class) {
-                currObj = Factory.getUserDao();
-            }
-            if (currObj.getClass().isAnnotationPresent(Dao.class)) {
-                field.set(instance, currObj);
-            } else {
-                throw new WrongAnnotationException("No annotation in this class ");
+            if (field.getAnnotation(Inject.class) != null) {
+                field.setAccessible(true);
+                Predicate predicate = o -> !(Factory.getBetDao().getClass()
+                        .isAnnotationPresent(Dao.class)
+                        || !(Factory.getUserDao().getClass()
+                        .isAnnotationPresent(Dao.class)));
+                if (predicate.test(field)) {
+                    throw new WrongAnnotationException("No such class annotation");
+                }
+                if (field.getType() == UserDao.class) {
+                    field.set(instance, Factory.getUserDao());
+                } else if (field.getType() == BetDao.class) {
+                    field.set(instance, Factory.getBetDao());
+                }
             }
         }
         return instance;
+    }
+
+    @Override
+    public Class<? extends Annotation> annotationType() {
+        return null;
     }
 }
